7274f3d6118ab9c3800f1c4155962c685b9dd405
cascateer <cascateer@gmail.com>
2026-06-16 19:28:53 +0200

    enumerator test

---
+++ src/Enumerable.test.ts
@@ -0,0 +1,15 @@
+import { expect, test } from "vitest";
+import { Enumerable, Enumerator } from "./Enumerable";
+
+test("enumerator", () => {
+  const array = [{ id: 2 }, { id: 3 }, { id: 5 }];
+  const enumerator: Enumerator<typeof array> = (item, index) =>
+    [index, item.id].join("-");
+
+  expect(array.map(enumerator)).toEqual(["0-2", "1-3", "2-5"]);
+});
+
+test("enumerable", () => {
+  expect(new Enumerable([2, 3, 5])).toEqual([2, 3, 5]);
+  expect(new Enumerable({ a: 2, b: 3, c: 5 })).toEqual([]);
+});
