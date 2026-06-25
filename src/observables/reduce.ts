b798d7780837e244970199b00c49d29b9d59925f
cascateer <cascateer@gmail.com>
2026-06-25 11:35:20 +0200

    reduce simplified

--- src/observables/reduce.ts
+++ src/observables/reduce.ts
@@ -1,24 +1,23 @@
-import { thru } from "lodash";
 import { map, OperatorFunction, scan } from "rxjs";
 
 export const reduce =
   <Event, Result = Event>(
-    predicate: (output: Result, ...events: [Event, ...Event[]]) => Result,
+    predicate: (output: Result, event: Event, previousEvent?: Event) => Result,
     seed: (event: Event) => Result,
   ): OperatorFunction<Event, Result> =>
   (source) =>
     source.pipe(
       scan(
-        ({ outputs, events: previousEvents }, event) =>
-          thru([event, ...previousEvents] as const, (events) => ({
-            outputs: [
-              0 in outputs ? predicate(outputs[0], ...events) : seed(event),
-            ],
-            events,
-          })),
-        {
-          outputs: new Array<Result>(),
-          events: [...new Array<Event>()] as const,
+        ({ outputs, previousEvent }, event) => ({
+          outputs: [
+            0 in outputs
+              ? predicate(outputs[0], event, previousEvent)
+              : seed(event),
+          ],
+          previousEvent: event,
+        }),
+        <{ outputs: Result[]; previousEvent?: Event }>{
+          outputs: [],
         },
       ),
       map(({ outputs }) => {
