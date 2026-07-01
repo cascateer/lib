6f6c214e4363f7678f0a7886eb6e30676e2680e1
cascateer <cascateer@gmail.com>
2026-07-01 19:47:10 +0200

    keyMapBy ex

--- src/keyMapBy.ts
+++ src/keyMapBy.ts
@@ -1,9 +1,16 @@
-import { Function1, keyBy, tap } from "lodash";
+import { Function1, Function3, keyBy, tap } from "lodash";
 
-export const keyMapBy = <T, U>(collection: T[], iteratee: Function1<T, U>) =>
+export const keyMapBy = <T, K, V>(
+  collection: T[],
+  keyIteratee: Function1<T, K>,
+  valueIteratee: Function3<T, number, Map<K, V>, V>,
+) =>
   collection.reduce(
-    (map, value) => tap(map, (map) => map.set(iteratee(value), value)),
-    new Map<U, T>(),
+    (map, item, index) =>
+      tap(map, (map) =>
+        map.set(keyIteratee(item), valueIteratee(item, index, map)),
+      ),
+    new Map<K, V>(),
   );
 
 keyBy;
