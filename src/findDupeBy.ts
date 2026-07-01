9abd42f3cf7b017c09bae3e7cc612ef04decf8c2
cascateer <cascateer@gmail.com>
2026-07-01 19:13:22 +0200

    findDupeBy

--- src/findDupeBy.ts
+++ src/findDupeBy.ts
@@ -1,9 +1,9 @@
 import { Function1, identity } from "lodash";
 
-export const findDupeBy = <T>(
+export const findDupeBy = <T, U>(
   items: T[],
-  iteratee: Function1<T, unknown> = identity,
-) =>
+  iteratee: Function1<T, U> = identity,
+): U | undefined =>
   items
     .map((item) => iteratee(item))
     .find((item, index, items) => index !== items.lastIndexOf(item));
