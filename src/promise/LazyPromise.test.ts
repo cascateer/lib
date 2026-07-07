3e442fa489976334797131e6ecfbedafa3e9545c
cascateer <cascateer@gmail.com>
2026-07-07 12:39:27 +0200

    test improved

--- src/promise/LazyPromise.test.ts
+++ src/promise/LazyPromise.test.ts
@@ -4,19 +4,21 @@ import { LazyPromise } from "./LazyPromise";
 test("LazyPromise rejection", () => {
   type Fruit = "apple" | "banana" | "tomato";
 
-  LazyPromise.concatAll(
-    ["apple" as const, "banana" as const, "tomato" as const].map(
-      (fruit) =>
-        new LazyPromise<void, Fruit>(
-          () =>
-            new Promise((resolve, reject) => {
-              if (fruit === "banana") {
-                return setTimeout(() => reject(new Error(fruit)));
-              }
+  return expect(
+    LazyPromise.concatAll(
+      ["apple" as const, "banana" as const, "tomato" as const].map(
+        (fruit) =>
+          new LazyPromise<void, Fruit>(
+            () =>
+              new Promise((resolve, reject) => {
+                if (fruit === "banana") {
+                  return setTimeout(() => reject(new Error(fruit)));
+                }
 
-              return resolve(fruit);
-            }),
-        ),
+                return resolve(fruit);
+              }),
+          ),
+      ),
     ),
-  ).catch((error) => expect(error.message).toEqual("banana"));
+  ).rejects.toEqual(new Error("banana"));
 });
